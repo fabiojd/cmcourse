dac3d63c898baab7832365b9ca5f5941a86347c8
Fábio Dantas <fabiojdantas@ymail.com>
2018-05-16 21:19:37 -0300

    Projeto criado!

---
+++ src/main/java/com/fjdantas/conceptualmodelingcourse/ConceptualmodelingcourseApplication.java
@@ -0,0 +1,12 @@
+package com.fjdantas.conceptualmodelingcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceptualmodelingcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConceptualmodelingcourseApplication.class, args);
+	}
+}
