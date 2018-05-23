97cf2f3e73d2bdc2907d3663b949f8d426a26c86
Fábio Dantas <fabiojdantas@ymail.com>
2018-05-23 17:01:17 -0300

    Entities State and City included!

--- src/main/java/com/fjdantas/cmcourse/CmcourseApplication.java
+++ src/main/java/com/fjdantas/cmcourse/CmcourseApplication.java
@@ -8,9 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.fjdantas.cmcourse.domain.Category;
+import com.fjdantas.cmcourse.domain.City;
 import com.fjdantas.cmcourse.domain.Product;
+import com.fjdantas.cmcourse.domain.State;
 import com.fjdantas.cmcourse.repositories.CategoryRepository;
+import com.fjdantas.cmcourse.repositories.CityRepository;
 import com.fjdantas.cmcourse.repositories.ProductRepository;
+import com.fjdantas.cmcourse.repositories.StateRepository;
 
 /*
  * class implements interface CommandLineRunner for objects instantiation in the initialization of the application
@@ -25,7 +29,13 @@ public class CmcourseApplication implements CommandLineRunner {
 	private CategoryRepository categoryRepository;
 	
 	@Autowired
-	private ProductRepository productrepository;
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private StateRepository stateRepository;
+	
+	@Autowired
+	private CityRepository cityRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CmcourseApplication.class, args);
@@ -39,16 +49,16 @@ public class CmcourseApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		//instantiating of the categories
+		//instantiating of the objects of categories
 		Category cat1 = new Category(null, "Computing");
 		Category cat2 = new Category(null, "Office");
 		
-		//instantiating of the products
+		//instantiating of the objects of products
 		Product p1 = new Product(null, "Computer", 2000.00);
 		Product p2 = new Product(null, "Printer", 800.00);
 		Product p3 = new Product(null, "Mouse", 80.00);
 		
-		//associating the products elements  with your category list
+		//associating the products elements with your category list
 		p1.getCategories().addAll(Arrays.asList(cat1));
 		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategories().addAll(Arrays.asList(cat1));
@@ -57,9 +67,28 @@ public class CmcourseApplication implements CommandLineRunner {
 		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProducts().addAll(Arrays.asList(p2));
 			
-		//saving a category object list in the database with a auto list into saveAll method
+		//saving a Category object list in the database with a auto list into saveAll method
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
-		//saving a product object list in the database with a auto list into saveAll method
-		productrepository.saveAll(Arrays.asList(p1, p2, p3));
+		//saving a Product object list in the database with a auto list into saveAll method
+		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		//instantiating of the objects of States
+		State st1 = new State(null, "Minas Gerais");
+		State st2 = new State(null, "São Paulo");
+		//Instantiating of the objects of Cities
+		City cit1 = new City(null, "Uberlândia", st1);
+		City cit2 = new City(null, "São Paulo", st2);
+		City cit3 = new City(null, "Campinas", st2);
+		
+		//associating the states elements with your cities list
+		st1.getCities().addAll(Arrays.asList(cit1));
+		st2.getCities().addAll(Arrays.asList(cit2, cit3));
+		
+		//saving a State object list in the database with a auto list into saveAll method
+		stateRepository.saveAll(Arrays.asList(st1, st2));
+		
+		//saving a City object list in the database with a auto list into saveAll method
+		cityRepository.saveAll(Arrays.asList(cit1, cit2, cit3));
+			
 	}
 }
